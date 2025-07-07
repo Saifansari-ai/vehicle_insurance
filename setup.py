ff74f37022c2c11720f093e83c47b9430f877b5c
Saifansari-ai <saifansari300b@gmail.com>
2025-07-07 16:22:15 +0530

    project structure done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mohd Saif Ansari",
+    author_email="saifansari300b@gmail.com",
+    packages=find_packages()
+)
